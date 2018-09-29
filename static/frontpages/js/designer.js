18691a29e9bc26c64ab37d3f63919eb77e2bfeb0
qiansongke <455988586@qq.com>
2018-09-29 10:06:42 +0800

    品创饰家官网 2018年9月29日 v0.0.2 1.case-in 更新css 更新填提交脚本 2.aboutus 更新侧边导航栏连接 3.scene 更新了一个弹出框 4.designer 更新表单提交脚本

--- static/frontpages/js/designer.js
+++ static/frontpages/js/designer.js
@@ -28,3 +28,44 @@ $(".buttons ul li a").hover(function () {
     $(".buttons ul li a").removeClass("pageNow");
     $(this).addClass("pageNow");
 })
+
+//表单验证
+$(".designer-intro-right-bottom input").click(function () {
+    $(this).attr("placeholder","");
+    clickNoPlaceholder($(".designer-intro-right-bottom"));
+})
+
+function clickNoPlaceholder (input){
+    var inputList = input;
+    inputList.click(function () {
+        $(this).attr("placeholder","");
+    })
+    inputList.eq(0).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "姓名")
+        }
+    })
+    inputList.eq(1).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "联系方式")
+        }
+    })
+}
+
+$(".designer-intro-right-bottom button").click(function () {
+    Validate($(".designer-intro-right-bottom input"))
+})
+function Validate(input) {
+    var inputList = input;
+    var regPhone = /^1[3|4|5|7|8][0-9]{9}$/;
+    if (inputList.eq(0).val() !== "") {
+
+    } else {
+        alert("请正确输入您的姓名！")
+    }
+    if (inputList.eq(1).val() !== "" && regPhone.test(inputList.eq(1).val())) {
+
+    } else {
+        alert("请输入正确的手机号码！")
+    }
+}
