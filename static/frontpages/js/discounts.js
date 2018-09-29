3f57aae454ac2262b9a29a91a0ce02966cfc2b1b
qiansongke <455988586@qq.com>
2018-09-29 10:15:40 +0800

    品创饰家官网 2018年9月29日 v0.0.2 1.case-in 更新css 更新填提交脚本 2.aboutus 更新侧边导航栏连接 3.scene 更新了一个弹出框 4.designer 更新表单提交脚本 5.discounts.js更新表单提交脚本

--- static/frontpages/js/discounts.js
+++ static/frontpages/js/discounts.js
@@ -1,2 +1,80 @@
+//表单验证
+$(".from-in-banner input").click(function () {
+    $(this).attr("placeholder","");
+    clickNoPlaceholder($(".from-in-banner"));
+})
 
+function clickNoPlaceholder (input){
+    var inputList = input;
+    inputList.click(function () {
+        $(this).attr("placeholder","");
+    })
+    inputList.eq(0).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "姓名")
+        }
+    })
+    inputList.eq(1).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "联系方式")
+        }
+    })
+}
+$(".from-in-banner button").click(function () {
+    Validate($(".from-in-banner input"))
+})
+function Validate(input) {
+    var inputList = input;
+    var regPhone = /^1[3|4|5|7|8][0-9]{9}$/;
+    if (inputList.eq(0).val() !== "") {
+
+    } else {
+        alert("请正确输入您的姓名！")
+    }
+    if (inputList.eq(1).val() !== "" && regPhone.test(inputList.eq(1).val())) {
+
+    } else {
+        alert("请输入正确的手机号码！")
+    }
+}
+
+//底部表单验证
+$(".bottom-form-right input").click(function () {
+    $(this).attr("placeholder","");
+    clickNoPlaceholder($(".bottom-form-right"));
+})
+
+function clickNoPlaceholder (input){
+    var inputList = input;
+    inputList.click(function () {
+        $(this).attr("placeholder","");
+    })
+    inputList.eq(0).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "姓名")
+        }
+    })
+    inputList.eq(1).blur(function () {
+        if ( $(this).val() === "") {
+            $(this).attr("placeholder", "联系方式")
+        }
+    })
+}
+$(".bottom-form-right button").click(function () {
+    Validate($(".bottom-form-right input"))
+})
+function Validate(input) {
+    var inputList = input;
+    var regPhone = /^1[3|4|5|7|8][0-9]{9}$/;
+    if (inputList.eq(0).val() !== "") {
+
+    } else {
+        alert("请正确输入您的姓名！")
+    }
+    if (inputList.eq(1).val() !== "" && regPhone.test(inputList.eq(1).val())) {
+
+    } else {
+        alert("请输入正确的手机号码！")
+    }
+}
 
